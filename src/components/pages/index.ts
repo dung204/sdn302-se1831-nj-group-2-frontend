8ac1776209f1e34981d761aac869803d3fbaa31b
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2025-03-26 13:13:45 +0700

    feat: add export for guest branches ID computers page

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -26,3 +26,4 @@ export * from './manage-deleted-usage-tracking.page';
 export * from './manage-users.page';
 export * from './manage-deleted-users.page';
 export * from './guest-branches.page';
+export * from './guest-branches-id-computers.page';
